708a07ad7d4b0a0c484e35d00ada2ecd4b30f67c
EkremTaskin <eko20042004@hotmail.com>
2021-10-19 00:21:18 +0300

    18.10.2021

---
+++ src/app.js
@@ -0,0 +1,25 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarBugun = 9.20
+
+
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11
+
+console.log(euroDun)
+
+//array
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+
+for (let index = 0; index < konutKredileri.length; index++) {
+    console.log(konutKredileri[index])
+}
+console.log(konutKredileri)
