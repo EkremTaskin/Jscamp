4b94ff11eca6e09d8563f9836f5a4fbc7d71b3e5
EkremTaskin <eko20042004@hotmail.com>
2021-11-22 23:17:11 +0300

    22.11.2021

--- src/crossCuttingConcerns/logger.js
+++ src/crossCuttingConcerns/logger.js
@@ -1,5 +1,17 @@
-export default class Logger{
+export class BaseLogger{
     log(data){
-        console.log(data + " Loglandı")
+        console.log("Default logger : " + data)
+    }
+}
+
+export class ElasticLogger extends BaseLogger{
+    log(data){
+        console.log("Logged to Elastic" + data)
+    }
+}
+
+export class MongoLogger extends BaseLogger{
+    log(data){
+        console.log("Logged to Mongo" + data)
     }
 }
